38bff51c2641c583bf264ff2de23c634ebf9e4c3
Dmitriy Rovenskiy <arhitec@bk.ru>
2021-12-09 22:48:01 +0300

    initial state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Select from 'react-select';
+
+export const options = [
+  { value: 'ocean', label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', color: '#00B8D9', isFixed: true },
+  { value: 'blue', label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', color: '#0052CC', isDisabled: true },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'grid', margin: '20px 700px'}}>
+      <Select options={options}/>
     </div>
   );
 }
